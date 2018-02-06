8ad9716f7da1d92521f362f9975a220b30115dfd
Mathéo <matheo.zeller@gmail.com>
2018-02-06 11:27:24 +0100

    First commit , initialisation du html + css + js

---
+++ dev/app/app.js
@@ -0,0 +1,13 @@
+// READ ME : NE PAS UTILISER LES MEMES VARIABLES POUR UTILISATON DIFFERENTE ,
+// RESPECT DES ZONES DE CODE POUR MOINS DE MERGES
+
+(function main () {
+// Code Céline
+  console.log('code ici')
+// Code Corentin
+  console.log('code ici')
+// Code Christelle
+  console.log('code ici')
+// Code Mathéo
+  console.log('code ici')
+})()
